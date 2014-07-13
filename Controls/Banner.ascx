e3af4b6c25a17a146d4d6ee5891eb583d93427ba
SeanTRobinson <Sean.T.Robinson@googlemail.com>
2014-07-13 18:46:20 +0100

    Banner added with User Control

---
+++ Controls/Banner.ascx
@@ -0,0 +1,14 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="Banner.ascx.cs" Inherits="Controls_Banner" %>
+
+<asp:Panel ID="VerticalPanel" runat="server">
+  <a href="http://p2p.wrox.com" target="_blank">
+    <asp:Image ID="Image1" runat="server" AlternateText="This is a sample banner" ImageUrl="~/Images/Banner120x240.gif" BorderStyle="None" />
+  </a>
+</asp:Panel>
+
+<asp:Panel ID="HorizontalPanel" runat="server">
+  <a href="http://p2p.wrox.com" target="_blank">
+    <asp:Image ID="Image2" runat="server" AlternateText="This is a sample banner" ImageUrl="~/Images/Banner468x60.gif" BorderStyle="None" />
+  </a>
+</asp:Panel>
+
---
+++ Controls/Banner.ascx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Controls_Banner : System.Web.UI.UserControl
+{
+  public Direction DisplayDirection { get; set; }
+
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    HorizontalPanel.Visible = false;
+    VerticalPanel.Visible = false;
+
+    switch(DisplayDirection)
+    {
+      case Direction.Horizontal:
+        HorizontalPanel.Visible = true;
+        break;
+      case Direction.Vertical:
+        VerticalPanel.Visible = true;
+        break;
+    }
+
+  }
+}
