b0c02b6aea0f04bbc364e8c441d7a1b8fa2263e2
SeanTRobinson <Sean.T.Robinson@googlemail.com>
2014-07-14 21:29:16 +0100

    Contact form added to site
    Data is validated at both client and server.

---
+++ Demos/Email.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+public partial class Demos_Email : BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      MailMessage myMessage = new MailMessage();
+      myMessage.Subject = "Test Message";
+      myMessage.Body = "Hello World, front Planet Wrox!";
+      myMessage.From = new MailAddress("sean@seantrobinson.co.uk", "Sender Name");
+      myMessage.To.Add(new MailAddress("sean.t.robinson@googlemail.com", "Reciever Name"));
+      SmtpClient mySmtpClient = new SmtpClient();
+      mySmtpClient.Send(myMessage);
+    }
+}
